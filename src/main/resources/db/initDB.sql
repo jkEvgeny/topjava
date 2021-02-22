ddd8f0390242efc12b39b60e7e7940a24e9b20d5
jkEvgeny <jk.galchenko@yandex.ru>
2021-02-22 23:53:05 +0300

    HW03 DB working create table meals & insert into meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS meals_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE meals_seq START WITH 200000;
 
 CREATE TABLE users
 (
@@ -18,8 +21,19 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
-    user_id INTEGER NOT NULL,
+    user_id INTEGER PRIMARY KEY NOT NULL,
     role    VARCHAR,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('meals_seq'),
+    dateTime         TIMESTAMP                               NOT NULL,
+    description      VARCHAR                                 NOT NULL,
+    calories         INTEGER             DEFAULT 2000        NOT NULL,
+    excess           BOOL                DEFAULT FALSE       NOT NULL,
+    user_id          INTEGER
+);
+CREATE UNIQUE INDEX meals_unique_dateTime_idx ON meals (dateTime, user_id);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,9 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE meals_seq RESTART WITH 200000;
+
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +12,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals ( datetime, description, calories)
+VALUES (now(), 'Завтрак', 500),
+       (now()+'1 minute', 'Обед', 1000),
+       (now()+'2 minute', 'Ужин', 500),
+       (now()+'3 minute','Еда на граничное значение', 100),
+       (now()+'4 minute','Завтрак', 1000),
+       (now()+'5 minute','Обед', 500),
+       (now()+'6 minute','Ужин', 410),
+       (now()+'2 minute','Админ ланч', 510),
+       (now()+'3 minute','Админ ужин', 1500);
